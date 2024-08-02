5e89a6aa2f0de56c84e11354f84b1cd9a2035acc
riaz57 <sharifriaz57@gmail.com>
2024-08-02 21:39:53 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("api.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log(`server running on port: ${port}`);
+});
