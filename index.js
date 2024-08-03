d02bbfd5d56f5f3c4b3e96830da1ba850e3d8b3a
riaz57 <sharifriaz57@gmail.com>
2024-08-03 22:47:59 +0600

    node api added

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 import cors from 'cors';
 import express from 'express';
 import data from './api.json' assert { type: 'json' };
-// const server = jsonServer.create();
-// const router = jsonServer.router("api.json");
-// const middlewares = jsonServer.defaults();
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
 
-// server.use(middlewares);
-// app.use(router);
+
+
 app.get('/categories', async (req, res, next) => {
-    return res.status(500).json(data.categories);
+    return res.status(200).json(data.categories);
 })
+app.get('/category/:id', async (req, res, next) => {
+    const { id } = req.params;
+    const filteredData = data.categories.filter(item => item.id == id);
+    return res.status(200).json(filteredData);
+})
+app.get('/products', async (req, res, next) => {
+    if (req.query.category_id) {
+        const categoryId = parseInt(req.query.category_id);
+
+        if (isNaN(categoryId)) {
+            return res.status(400).json({ error: 'Invalid category_id' });
+        }
+        const filteredData = data.products.filter(item => item.id == categoryId);
+        return res.status(200).json(filteredData);
+    }
+
+    return res.status(200).json(data.products);
+})
+app.get(`/product/:id`, async (req, res, next) => {
+    const { id } = req.params;
+    const filteredData = data.products.filter(item => item.id == id);
+    return res.status(200).json(filteredData);
+})
+app.get('/filter', async (req, res, next) => {
+    if (req.query.name) {
+        const name = req.query.name;
+
+        const filteredProducts = data.products.filter(item => item.name.toLowerCase().includes(name.toLowerCase()));
+        const filteredCategories = data.categories.filter(item => item.name.toLowerCase().includes(name.toLowerCase()));
+        return res.status(200).json({
+            "categories": filteredCategories,
+            "products": filteredProducts,
+        });
+    }
+    return res.status(200).json({
+        "categories": data.categories,
+        "products": data.products,
+    });
+})
+
 
 app.listen(port, () => {
     console.log(`server running on port: ${port}`);
